84e7d273fac45ec70ce526a86d69b82c5ade1b41
juhanse <julien.hanse12@gmail.com>
2025-03-04 14:35:31 +0100

    add: ex00 megaphone.c

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 14:10:06 by juhanse           #+#    #+#             */
+/*   Updated: 2025/03/04 14:33:59 by juhanse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	i = 0;
+	while (ac > 1 && ++i < ac)
+	{
+		j = -1;
+		while (av[i][++j])
+			std::cout << (char)std::toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
