211838da04cf81d8ff09cb612a913ffbf77ef38b
juhanse <julien.hanse12@gmail.com>
2025-04-17 22:05:38 +0200

    fix: megaphone cpp

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -18,9 +18,12 @@ int	main(int ac, char **av)
 	int	j;
 
 	if (ac < 2)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return (0);
+	}
 	i = 0;
-	while (ac > 1 && ++i < ac)
+	while (++i < ac)
 	{
 		j = -1;
 		while (av[i][++j])
