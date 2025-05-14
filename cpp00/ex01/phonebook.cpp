1e717b304156734f1b673a2c504b0f57f671a109
juhanse <julien.hanse12@gmail.com>
2025-05-14 14:28:48 +0200

    fix: phonebook class

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -6,44 +6,89 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 16:50:08 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/14 12:11:28 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/14 14:27:49 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-void	PhoneBook::add(void)
-{
-	int	current;
+PhoneBook::PhoneBook() : size(0), oldestIndex(0) {};
 
-	if (isFull)
-		std::cout << "Phonebook is full! Overwriting oldest contact...\n";
-	std::string first, last, nickname, dark, number;
-	std::cout << "Please add a First Name : ";
-	std::getline(std::cin, first);
-	std::cout << "Please add a Last Name : ";
-	std::getline(std::cin, last);
-	std::cout << "Please add a Nick Name: ";
-	std::getline(std::cin, nickname);
-	std::cout << "Please add a Phone NUmber : ";
-	std::getline(std::cin, number);
-	std::cout << "Please add a your Darkest secret : ";
-	std::getline(std::cin, dark);
-	current = nb_contacts + 1 % 8;
-	contacts[current].add(first, last, nickname, dark, number);
-	nb_contacts++;
-	std::cout << "\nSuccessfully added your info!\n";
+void	PhoneBook::add(void) {
+	std::string first, last, nick, dark, number;
+
+	do {
+		std::cout << "Firstname: ";
+		std::getline(std::cin, first);
+		if (first.empty())
+			std::cout << "Firstname cannot be empty" << std::endl;
+	} while (first.empty());
+
+	do {
+		std::cout << "Lastname: ";
+		std::getline(std::cin, last);
+		if (last.empty())
+			std::cout << "Lastname cannot be empty" << std::endl;
+	} while (last.empty());
+
+	do {
+		std::cout << "Nickname: ";
+		std::getline(std::cin, nick);
+		if (nick.empty())
+			std::cout << "Nickname cannot be empty" << std::endl;
+	} while (nick.empty());
+
+	do {
+		std::cout << "Phone number: ";
+		std::getline(std::cin, number);
+		if (number.empty())
+			std::cout << "Phone number cannot be empty" << std::endl;
+	} while (number.empty());
+
+	do {
+		std::cout << "Darkest secret: ";
+		std::getline(std::cin, dark);
+		if (dark.empty())
+			std::cout << "Darkest secret be empty" << std::endl;
+	} while (dark.empty());
+
+	contacts[oldestIndex].setContact(first, last, nick, dark, number);
+	oldestIndex = (oldestIndex + 1) % 8;
+	if (size < 8)
+		size++;
 }
 
-void	PhoneBook::search(void)
-{
-	int	index;
+void	PhoneBook::search(void) const {
+	std::string input;
 
-	contacts->displayAll();
+	if (size > 0)
+	{
+		std::cout << std::setw(10) << "Index" << "|"
+		<< std::setw(10) << "Firstname" << "|"
+		<< std::setw(10) << "Lastname" << "|"
+		<< std::setw(10) << "Nickname" << "|"
+		<< std::setw(10) << "Darkest" << "|"
+		<< std::setw(10) << "Number" << std::endl;
+
+		for (int i = 0; i < size; i++) {
+			contacts[i].display(i);
+		}
+	}
+
 	std::cout << "Enter the index of the contact you want to view: ";
-	std::getline(std::cin, index);
-	if (index < 0 || index > nb_contacts)
-		std::cout << "Invalid index";
+	std::getline(std::cin, input);
+
+	if (input.length() != 1 || !isdigit(input[0])) {
+		std::cout << "Invalid index" << std::endl;
+		return;
+	}
+
+	int index = input[0] - '0';
+	if (index < 0 || index >= size) {
+		std::cout << "Invalid index" << std::endl;
+		return;
+	}
+
 	Contact contact = contacts[index];
-	contact.display(contact);
+	contacts[index].displayContact(contact);
 }
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 21:07:52 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/14 12:08:17 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/14 14:27:44 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@ class PhoneBook
 {
 	public :
 		Contact	contacts[8];
-		bool	isFull;
-		int		nb_contacts;
+		int		size;
+		int		oldestIndex;
 	public :
+		PhoneBook();
+
 		void	add(void);
-		void	search(void);
+		void	search(void) const;
 };
 
 #endif
