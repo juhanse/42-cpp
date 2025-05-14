7439970c317fdf5a0801a371f06a8869e6214ba4
juhanse <julien.hanse12@gmail.com>
2025-05-14 13:39:39 +0200

    add: terminal protection

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,27 +6,31 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 21:11:56 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/14 11:11:35 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/14 13:38:56 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-int	main(void)
+int	main(int ac, char **av)
 {
+	std::string	input;
 	PhoneBook	phonebook;
-	std::string	cmd;
 
-	phonebook.current = 0;
+	(void)av;
+	if (ac != 1)
+		return (0);
 	while (true)
 	{
-		std::cout << "\nEnter a command : ADD or SEARCH or EXIT \n >";
-		std::getline(std::cin, cmd);
-		if (cmd == "ADD")
+		if (std::cin.eof())
+			break ;
+		std::cout << "Enter command [ADD  | SEARCH | EXIT]: ";
+		std::getline(std::cin, input);
+		if (input == "ADD")
 			phonebook.add();
-		else if (cmd == "SEARCH")
+		else if (input == "SEARCH")
 			phonebook.search();
-		else if (cmd == "EXIT")
+		else if (input == "EXIT")
 			break ;
 	}
 	return (0);
