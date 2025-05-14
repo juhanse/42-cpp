5223d89587b104eb414db911704c8ba592b5b534
juhanse <julien.hanse12@gmail.com>
2025-05-14 11:29:47 +0200

    add: contact hpp

---
+++ cpp00/ex01/contact.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 10:56:59 by juhanse           #+#    #+#             */
+/*   Updated: 2025/05/14 11:26:09 by juhanse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <cstring>
+
+class Contact
+{
+	private:
+		std::string	firstname;
+		std::string	lastname;
+		std::string	nickname;
+		std::string	darkest;
+		std::string	number;
+
+		static std::string truncate(const std::string& str);
+	
+	public:
+		Contact();
+	
+		void setContact(const std::string firstname, const std::string lastname, const std::string nickname, const std::string darkest, const std::string number);
+		void display(int index) const;
+		void displayAll() const;
+};
+
+#endif
