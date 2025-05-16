430e8c86114ea88ffd878a5ebe208ba6406d0984
juhanse <julien.hanse12@gmail.com>
2025-05-16 21:25:10 +0200

    add: randomChump method

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,8 +6,13 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 15:29:58 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/16 15:30:02 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/16 21:20:54 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+void randomChump(std::string name) {
+	Zombie zombie(name);
+	zombie.announce();
+}
