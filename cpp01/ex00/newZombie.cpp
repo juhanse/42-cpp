583cab3b29c5283e8bd2c17a6c0bd0a71c3e06ab
juhanse <julien.hanse12@gmail.com>
2025-05-16 21:24:59 +0200

    add: newZombie method

--- cpp01/ex00/newZombie.cpp
+++ cpp01/ex00/newZombie.cpp
@@ -6,8 +6,13 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 15:30:05 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/16 15:30:09 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/16 21:24:02 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie* newZombie(std::string name) {
+	Zombie* zombie = new Zombie(name);
+	return (zombie);
+}
