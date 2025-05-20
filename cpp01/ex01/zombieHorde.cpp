7f07cc1d7db779b704106d708e60346b20821a1e
juhanse <julien.hanse12@gmail.com>
2025-05-20 13:56:30 +0200

    fix: zombieHorde file

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/20 13:40:43 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/20 13:54:59 by juhanse          ###   ########.fr       */
+/*   Created: 2025/05/20 13:54:33 by juhanse           #+#    #+#             */
+/*   Updated: 2025/05/20 13:54:50 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie() {}
+Zombie	*zombieHorde(int N, std::string name) {
+	if (N <= 0)
+		return (NULL);
 
-Zombie::Zombie(std::string name) {
-	this->_name = name;
-}
+	Zombie	*horde = new Zombie[N];
+	for (int i = 0; i < N; i++)
+	{
+		horde[i].setName(name);
+	}
 
-Zombie::~Zombie() {
-	std::cout << "Zombie " << _name << " is destroyed." << std::endl;
-}
-
-void Zombie::setName(std::string name) {
-	_name = name;
-}
-
-void Zombie::announce() const {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	return (horde);
 }
