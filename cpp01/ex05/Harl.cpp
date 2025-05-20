6b8ceb5dc7ffd19b8be420b59a23c7c67972a56f
juhanse <julien.hanse12@gmail.com>
2025-05-20 20:30:02 +0200

    add: private methods

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:49:23 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/20 16:52:46 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/20 20:29:38 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,20 @@ Harl::~Harl() {
 
 void	complain(std::string level) {
 	
+}
+
+void	debug(void) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	info(void) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	warning(void) {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void	error(void) {
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
