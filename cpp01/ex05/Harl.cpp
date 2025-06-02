54f74e8fe063c13696b498e293bb0261c139c114
juhanse <julien.hanse12@gmail.com>
2025-06-02 15:06:16 +0200

    add: main file

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:49:23 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/20 20:29:38 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/06/02 15:05:27 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,15 @@ Harl::~Harl() {
 	std::cout << "Harl is destroyed" << std::endl;
 }
 
-void	complain(std::string level) {
-	
-}
+void	Harl::complain(std::string level) {
+	int 	i = 0;
+	void	(Harl::*complaints[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-void	debug(void) {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
-}
-
-void	info(void) {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
-}
-
-void	warning(void) {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
-}
-
-void	error(void) {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	while (i++ < 4) {
+		if (level == levels[i]) {
+			(this->*complaints[i])();
+			return ;
+		}
+	}
 }
--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:49:18 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/20 16:55:53 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/06/02 14:51:21 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,27 @@
 # include <string>
 
 class Harl {
-	private:
-		void	debug(void);
-		void	info(void);
-		void	warning(void);
-		void	error(void);
-
 	public:
 		Harl();
 		~Harl();
-
 		void	complain(std::string level);
+
+	private:
+		void	debug(void) {
+			std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+		}
+
+		void	info(void) {
+			std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+		}
+
+		void	warning(void) {
+			std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+		}
+
+		void	error(void) {
+			std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+		}
 };
 
 #endif
