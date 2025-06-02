7db4109013906a71cda36cb010fe6dd9461a024e
juhanse <julien.hanse12@gmail.com>
2025-06-02 15:13:18 +0200

    add: intput & output file checker

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,13 +6,14 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:47:06 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/20 16:45:27 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/06/02 15:12:52 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
+#include <cstring>
 
 int main(int ac, char **av)
 {
@@ -27,21 +28,20 @@ int main(int ac, char **av)
 	std::string s2 = av[3];
 
 	std::ifstream input(filename.c_str());
+	if (input.good() == false) {
+		std::cout << "File not found" << std::endl;
+		return (1);
+	}
 	std::ofstream output((filename + ".replace").c_str());
+	if (output.good() == false) {
+		std::cout << "Could not create output file" << std::endl;
+		input.close();
+		return (1);
+	}
+	std::string buffer;
+	while (getline(input, buffer))
+		// function
 
-	std::string line;
-	int	i = 0;
-	while (getline(input, line))
-	{
-		i++;
-		if (line.find(s1))
-		{
-			line.replace(i, s1.length(), s2);
-		}
-		std::cout << line << std::endl;
-	}	
-
-	output << input.rdbuf();
 	input.close();
 	output.close();
 	return (0);
