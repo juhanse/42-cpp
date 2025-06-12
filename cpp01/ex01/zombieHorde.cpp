29fd4e152b79f139d47a563b7fad6d0801ae13cb
juhanse <julien.hanse12@gmail.com>
2025-06-12 13:51:35 +0200

    fix: clean code

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:54:33 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/20 13:54:50 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/06/12 13:50:35 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ Zombie	*zombieHorde(int N, std::string name) {
 		return (NULL);
 
 	Zombie	*horde = new Zombie[N];
-	for (int i = 0; i < N; i++)
-	{
+	for (int i = 0; i < N; i++) {
 		horde[i].setName(name);
 	}
 
