94c5c53cf9c7ff198ee7678bdec14423ffc2a018
juhanse <julien.hanse12@gmail.com>
2025-06-12 12:02:21 +0200

    fix: getline operation

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:47:06 by juhanse           #+#    #+#             */
-/*   Updated: 2025/06/02 15:12:52 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/06/12 12:01:05 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <string>
 #include <fstream>
 #include <cstring>
+#include <sstream>
 
 int main(int ac, char **av)
 {
@@ -32,15 +33,27 @@ int main(int ac, char **av)
 		std::cout << "File not found" << std::endl;
 		return (1);
 	}
+
+	std::string line;
+	std::string content;
+	while (std::getline(input, line)) {
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != std::string::npos) {
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+		}
+		line += '\n';
+		content.append(line);
+	}
+
 	std::ofstream output((filename + ".replace").c_str());
+	output << content;
 	if (output.good() == false) {
 		std::cout << "Could not create output file" << std::endl;
 		input.close();
 		return (1);
 	}
-	std::string buffer;
-	while (getline(input, buffer))
-		// function
 
 	input.close();
 	output.close();
