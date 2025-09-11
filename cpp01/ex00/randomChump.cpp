66fc315abd5b159c01de22acb74d14f7bc4bad29
juhanse <julien.hanse12@gmail.com>
2025-09-11 18:29:55 +0200

    add: randomChump file

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/16 15:25:14 by juhanse           #+#    #+#             */
-/*   Updated: 2025/09/11 18:29:04 by juhanse          ###   ########.fr       */
+/*   Created: 2025/09/11 18:28:54 by juhanse           #+#    #+#             */
+/*   Updated: 2025/09/11 18:29:15 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie() {}
-
-Zombie::Zombie(const std::string& name) {
-	this->_name = name;
-	std::cout << "Zombie \"" << _name << "\" created" << std::endl;
-}
-
-Zombie::~Zombie() {
-	std::cout << "Zombie " << _name << " is destroyed." << std::endl;
-}
-
-void Zombie::announce(void) const {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+void randomChump(const std::string& name) {
+	Zombie zombie(name);
+	zombie.announce();
 }
