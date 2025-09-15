c98a7cb75316c827be9debda860e5e2a82f93e2d
juhanse <julien.hanse12@gmail.com>
2025-09-15 10:10:29 +0200

    fix: clean main

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,15 +6,15 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/07/07 12:43:30 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/09/15 10:08:50 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int	main( void ) {
+int	main(void) {
 	Fixed a;
-	Fixed b( a );
+	Fixed b(a);
 	Fixed c;
 
 	c = b;
@@ -23,5 +23,5 @@ int	main( void ) {
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
 
-	return 0;
+	return (0);
 }
