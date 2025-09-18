76a0793cf0b13fb9e37e86319efcda639e4e8bd1
juhanse <julien.hanse12@gmail.com>
2025-09-18 13:31:25 +0200

    fix: unsigned int protected var

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:35 by juhanse           #+#    #+#             */
-/*   Updated: 2025/09/15 12:01:12 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/09/18 13:30:51 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,33 @@
 # include <string>
 
 class ClapTrap {
-	private:
-		std::string	_name;
-		int			_hitPoints;
-		int			_energyPoints;
-		int			_attackDamage;
+	protected:
+		std::string		name;
+		unsigned int	hitPoints;
+		unsigned int	energyPoints;
+		unsigned int	attackDamage;
 
 	public:
 		ClapTrap(void);
 		ClapTrap(const std::string& name);
-		~ClapTrap(void);
+		ClapTrap(const ClapTrap& other);
+		virtual ~ClapTrap(void);
 
 		ClapTrap& operator=(const ClapTrap& other);
 
-		void attack(const std::string& target);
+		virtual void attack(const std::string& target);
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
 
 		std::string getName(void) const;
-		int getHitPoints(void) const;
-		int getEnergyPoints(void) const;
-		int getAttackDamage(void) const;
+		unsigned int getHitPoints(void) const;
+		unsigned int getEnergyPoints(void) const;
+		unsigned int getAttackDamage(void) const;
+	
+		void setName(const std::string& name);
+		void setHitPoints(unsigned int hitPoints);
+		void setEnergyPoints(unsigned int energyPoints);
+		void setAttackDamage(unsigned int attackDamage);
 };
 
 #endif
