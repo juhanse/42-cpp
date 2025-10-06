5ed13826cf0ff985e4ede49a00a25f8c02fa241f
juhanse <julien.hanse12@gmail.com>
2025-10-06 17:55:19 +0200

    add: all methods for canonical form

--- cpp04/ex00/Cat.hpp
+++ cpp04/ex00/Cat.hpp
@@ -6,19 +6,23 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 11:51:04 by juhanse           #+#    #+#             */
-/*   Updated: 2025/09/16 13:43:18 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/06 17:50:02 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAT_HPP
 # define CAT_HPP
 
-# include "Animal.hpp"
+#include "Animal.hpp"
 
 class Cat : public Animal {
 	public:
-		Cat();
-		~Cat();
+		Cat(void);
+		Cat(const std::string& type);
+		Cat(const Cat& other);
+		~Cat(void);
+
+		Cat& operator=(const Cat& other);
 
 		void makeSound(void) const;
 };
