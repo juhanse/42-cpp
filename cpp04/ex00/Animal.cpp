4510be253c1e1aa48e33613f437bce63e3edefb7
juhanse <julien.hanse12@gmail.com>
2025-10-06 16:35:58 +0200

    fix: display message

--- cpp04/ex00/Animal.cpp
+++ cpp04/ex00/Animal.cpp
@@ -6,25 +6,24 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 11:50:49 by juhanse           #+#    #+#             */
-/*   Updated: 2025/09/16 13:47:40 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/06 16:35:40 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 
 Animal::Animal() : _type("Animal") {
-	std::cout << "Animal constructor has been created." << std::endl;
-	return ;
+	std::cout << "Constructor Animal created" << std::endl;
 }
 
 Animal::~Animal() {
-	std::cout << "Animal destructor called" << std::endl;
+	std::cout << "Destructor Animal called" << std::endl;
 }
 
 void Animal::makeSound(void) const {
-	std::cout << "Some generic animal sound" << std::endl;
+	std::cout << "Default animal sound" << std::endl;
 }
 
 std::string Animal::getType(void) const {
-	return _type;
+	return (this->_type);
 }
