e0d7c88a012d4fe647c4da39c0ad0b4bf26d05d8
juhanse <julien.hanse12@gmail.com>
2025-10-08 15:34:59 +0200

    add: enhanced main

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/07 18:18:39 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/08 15:33:50 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,32 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-int	main(void) {
-	const Animal* meta = new Animal();
-	const Animal* i = new Cat();
-	const Animal* j = new Dog();
-	std::cout << i->getType() << " " << std::endl;
-	std::cout << j->getType() << " " << std::endl;
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
-	i->makeSound();
-	j->makeSound();
-	meta->makeSound();
+int main(void) {
+    std::cout << "=== Polymorphism ===" << std::endl;
+    const Animal* a1 = new Animal();
+    const Animal* a2 = new Cat();
+    const Animal* a3 = new Dog();
 
-	delete meta;
-	delete i;
-	delete j;
+    std::cout << a1->getType() << " says: "; a1->makeSound();
+    std::cout << a2->getType() << " says: "; a2->makeSound();
+    std::cout << a3->getType() << " says: "; a3->makeSound();
 
-	return (0);
+    delete a1;
+    delete a2;
+    delete a3;
+
+    std::cout << "\n=== Wrong ===" << std::endl;
+    const WrongAnimal* w1 = new WrongAnimal();
+    const WrongAnimal* w2 = new WrongCat();
+
+    std::cout << w1->getType() << " says: "; w1->makeSound();
+    std::cout << w2->getType() << " says: "; w2->makeSound();
+
+    delete w1;
+    delete w2;
+
+    return (0);
 }
