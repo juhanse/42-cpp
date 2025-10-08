101ae8dbc5faf55c86e4a3ad4ffd81126c12e142
juhanse <julien.hanse12@gmail.com>
2025-10-08 16:15:34 +0200

    add: enhanced main

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/08 14:01:54 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/08 16:15:09 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,29 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-int	main(void) {
-	//const AAnimal* meta = new AAnimal();
-	const Cat* i = new Cat();
-	const Dog* j = new Dog();
-	std::cout << i->getType() << " " << std::endl;
-	std::cout << j->getType() << " " << std::endl;
+int main(void) {
+    Dog dog1;
+    Dog dog2;
+    Cat cat1;
+    Cat cat2;
 
-	i->makeSound();
-	j->makeSound();
+    dog1.getBrain()->setIdea(0, "Coucou dog1");
+    dog2 = dog1;
+    dog2.getBrain()->setIdea(0, "Coucou dog2");
 
-	delete i;
-	delete j;
+    cat1.getBrain()->setIdea(0, "Coucou cat1");
+    cat2 = cat1;
+    cat2.getBrain()->setIdea(0, "Coucou cat2");
 
-	return (0);
+    Dog dogs[] = { dog1, dog2 };
+    Cat cats[] = { cat1, cat2 };
+
+    for (int i = 0; i < 2; ++i) {
+        std::cout << dogs[i].getType() << " idea 0: " << dogs[i].getBrain()->getIdea(0) << std::endl;
+        std::cout << cats[i].getType() << " idea 0: " << cats[i].getBrain()->getIdea(0) << std::endl;
+        dogs[i].makeSound();
+        cats[i].makeSound();
+    }
+
+    return (0);
 }
