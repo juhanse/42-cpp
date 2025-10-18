dbea4ea060495a5e9433c402bb4ce1dcd8c97ec2
juhanse <julien.hanse12@gmail.com>
2025-10-18 21:28:48 +0200

    fix: update Bureaucrat class

--- cpp05/ex01/Bureaucrat.cpp
+++ cpp05/ex01/Bureaucrat.cpp
@@ -16,7 +16,7 @@ Bureaucrat::Bureaucrat() : _name("Default"), _grade(10) {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const std::string& name, unsigned int grade) : _name(name) {
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name) {
 	if (grade < 1) {
 		throw GradeTooHighException();
 	} else if (grade > 150) {
@@ -28,9 +28,8 @@ Bureaucrat::Bureaucrat(const std::string& name, unsigned int grade) : _name(name
 	std::cout << "Bureaucrat " << this->_name << " has been created with name constructor." << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat& copy) {
+Bureaucrat::Bureaucrat(const Bureaucrat& copy) : _name(copy._name), _grade(copy._grade) {
 	std::cout << "Copy constructor called" << std::endl;
-	*this = copy;
 }
 
 Bureaucrat::~Bureaucrat() {
@@ -40,8 +39,7 @@ Bureaucrat::~Bureaucrat() {
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other) {
-		this->_name = other.getName();
-		this->_grade = other.getGrade();
+		this->_grade = other._grade;
 	}
 	return *this;
 }
@@ -50,12 +48,8 @@ std::string Bureaucrat::getName(void) const {
 	return (this->_name);
 }
 
-unsigned int Bureaucrat::getGrade(void) const {
+int Bureaucrat::getGrade(void) const {
 	return (this->_grade);
-}
-
-void Bureaucrat::setName(const std::string& name) {
-	this->_name = name;
 }
 
 void Bureaucrat::promote(void) {
