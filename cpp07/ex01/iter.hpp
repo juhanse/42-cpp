3374488418aa84208fad1577a2bd0d768b7ee8b5
juhanse <julien.hanse12@gmail.com>
2025-10-21 00:35:34 +0200

    add: cpp07 ex01 iter

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 00:08:12 by juhanse           #+#    #+#             */
+/*   Updated: 2025/10/21 00:19:07 by juhanse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template <typename T>
+void	iter( T* array, size_t length, void (*func)(T&) ) {
+	for ( size_t i = 0; i < length; i++ ) {
+		func( array[i] );
+	}
+}
+
+#endif
