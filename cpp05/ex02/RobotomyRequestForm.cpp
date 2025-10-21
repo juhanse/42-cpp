76737f57a30097b97c90cecc1f4b7bffe7b4b9bb
juhanse <julien.hanse12@gmail.com>
2025-10-22 00:29:30 +0200

    add: RobotomyRequestForm cpp

---
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 00:25:22 by juhanse           #+#    #+#             */
+/*   Updated: 2025/10/22 00:25:22 by juhanse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : AForm("Robotomy Request", 72, 45), _target("Default") {}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string& target) : AForm("Robotomy Request", 72, 45), _target(target) {}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& copy) : AForm(copy), _target(copy._target) {}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm& other) {
+	if (this != &other) {
+		AForm::operator=(other);
+		this->_target = other._target;
+	}
+	return *this;
+}
+
+const std::string& RobotomyRequestForm::getTarget(void) const {
+	return (this->_target);
+}
+
+void RobotomyRequestForm::execute(const Bureaucrat& executor) const {
+	if (!this->getIsSigned()) {
+		throw AForm::FormNotSignedException();
+	}
+	if (executor.getGrade() > this->getGradeExecute()) {
+		throw AForm::GradeTooLowException();
+	}
+
+	if (rand() % 2) {
+		std::cout << this->_target << " has been robotomized successfully." << std::endl;
+	} else {
+		std::cout << "The robotomy on " << this->_target << " has failed." << std::endl;
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const RobotomyRequestForm& robotomy) {
+	os << "Robotomy Request Form - target: " << robotomy.getTarget();
+	return os;
+}
