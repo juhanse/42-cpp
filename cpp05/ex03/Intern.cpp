0f48826c15b96c868700669fe18ca07744745eb7
juhanse <julien.hanse12@gmail.com>
2025-10-22 15:49:43 +0200

    add: makeForm method

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -6,11 +6,14 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 00:40:53 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/22 15:27:09 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/22 15:49:08 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/Intern.hpp"
+#include "includes/PresidentialPardonForm.hpp"
+#include "includes/RobotomyRequestForm.hpp"
+#include "includes/ShrubberyCreationForm.hpp"
 
 Intern::Intern() {}
 
@@ -25,5 +28,35 @@ Intern& Intern::operator=(const Intern& other) {
 	return *this;
 }
 
+AForm *Intern::createPresidentialPardonForm(const std::string& target) const {
+	return new PresidentialPardonForm(target);
+}
+
+AForm *Intern::createRobotomyRequestForm(const std::string& target) const {
+	return new RobotomyRequestForm(target);
+}
+
+AForm *Intern::createShrubberyCreationForm(const std::string& target) const {
+	return new ShrubberyCreationForm(target);
+}
+
 AForm *Intern::makeForm(const std::string& formName, const std::string& target) {
+    std::string formTypes[3] = {"presidential pardon", "robotomy request", "shrubbery creation"};
+    AForm* (Intern::*forms[3])(const std::string&) const = {
+        &Intern::createPresidentialPardonForm,
+        &Intern::createRobotomyRequestForm,
+        &Intern::createShrubberyCreationForm,
+    };
+
+	int i = -1;
+    while (++i < 3) {
+        if (formName == formTypes[i]) {
+            AForm* form = (this->*forms[i])(target);
+            std::cout << "Intern creates " << form->getName() << std::endl;
+            return form;
+        }
+    }
+
+    std::cout << "Form: " << formName << " invalid." << std::endl;
+    return (NULL);
 }
--- cpp05/ex03/includes/Intern.hpp
+++ cpp05/ex03/includes/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 00:39:01 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/22 15:25:41 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/22 15:48:48 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@
 # include "AForm.hpp"
 
 class Intern {
+	private:
+		AForm* createPresidentialPardonForm(const std::string& target) const;
+		AForm* createRobotomyRequestForm(const std::string& target) const;
+		AForm* createShrubberyCreationForm(const std::string& target) const;
+
 	public:
 		Intern();
 		Intern(const Intern& copy);
