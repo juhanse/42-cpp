b0f65369abb916ce673f67290648ba8a956dea3d
juhanse <julien.hanse12@gmail.com>
2025-10-22 16:05:23 +0200

    add: advanced test main

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/22 14:52:23 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/22 16:04:56 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,36 @@
 #include "Form.hpp"
 
 int	main(void) {
-	Bureaucrat michel("Michel", 2);
-	Form form("Constitution", 1, 2);
+	{
+		Bureaucrat michel("Michel", 2);
+		Form form("Constitution", 1, 2);
+	
+		std::cout << michel << std::endl;
+		std::cout << form << std::endl;
 
-	std::cout << michel << std::endl;
-	std::cout << form << std::endl;
+		std::cout << "END" << std::endl;
+	}
+	{
+		Bureaucrat michel("Michel", 2);
+		Form form("Constitution", 1, 2);
+	
+		std::cout << michel << std::endl;
+		std::cout << form << std::endl;
+
+		try {
+			michel.signForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			form.beSigned(michel);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		std::cout << "END" << std::endl;
+	}
 
 	return (0);
 }
