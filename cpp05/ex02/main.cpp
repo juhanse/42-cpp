6e4447010d4f84a8406af044bb965a85d03d2b05
juhanse <julien.hanse12@gmail.com>
2025-10-22 16:02:51 +0200

    add: advanced test main

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,19 +6,97 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/22 15:10:20 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/22 16:02:40 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/Bureaucrat.hpp"
 #include "includes/PresidentialPardonForm.hpp"
+#include "includes/RobotomyRequestForm.hpp"
+#include "includes/ShrubberyCreationForm.hpp"
 
 int	main(void) {
-	Bureaucrat michel("Michel", 2);
-	PresidentialPardonForm form("Michel");
+	{
+		Bureaucrat michel("Michel", 2);
+		PresidentialPardonForm form("Michel");
+	
+		std::cout << michel << std::endl;
+		std::cout << form << std::endl;
 
-	std::cout << michel << std::endl;
-	std::cout << form << std::endl;
+		try {
+			michel.executeForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			michel.signForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			michel.executeForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		std::cout << "END" << std::endl;
+	}
+	{
+		Bureaucrat michel("Michel", 2);
+		RobotomyRequestForm form("Michel");
+	
+		std::cout << michel << std::endl;
+		std::cout << form << std::endl;
+
+		try {
+			michel.executeForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			michel.signForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			michel.executeForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		std::cout << "END" << std::endl;
+	}
+	{
+		Bureaucrat michel("Michel", 2);
+		ShrubberyCreationForm form("Michel");
+	
+		std::cout << michel << std::endl;
+		std::cout << form << std::endl;
+
+		try {
+			michel.executeForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			michel.signForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			michel.executeForm(form);
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		std::cout << "END" << std::endl;
+	}
 
 	return (0);
 }
