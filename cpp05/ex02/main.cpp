c3095ee5b5a848555b3c08b1c382b82b4fd22784
juhanse <julien.hanse12@gmail.com>
2025-10-22 15:23:01 +0200

    fix: form definition

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,16 +6,16 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/15 18:29:18 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/10/22 15:10:20 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
-#include "Form.hpp"
+#include "includes/Bureaucrat.hpp"
+#include "includes/PresidentialPardonForm.hpp"
 
 int	main(void) {
 	Bureaucrat michel("Michel", 2);
-	Form form("Constitution", true, 1, 2);
+	PresidentialPardonForm form("Michel");
 
 	std::cout << michel << std::endl;
 	std::cout << form << std::endl;
