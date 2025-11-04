42eb05e1bdb95f906f474099d5cf0172d4cd4279
juhanse <julien.hanse12@gmail.com>
2025-11-04 13:58:04 +0100

    fix: public methods

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:35 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/30 23:46:40 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/11/04 13:55:05 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 # include <iostream>
 # include <string>
+# include <cstdint>
+# include <cstdlib>
 
 class ScalarConverter {
-	private:
-		static void	convert(const std::string& str);
-
 	public:
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter& copy);
 		~ScalarConverter();
 
 		ScalarConverter& operator=(const ScalarConverter& other);
+
+		static void	convert(const std::string& str);
 };
