01370dea1b9341001bb2f7cb75f50b0cbda48990
juhanse <julien.hanse12@gmail.com>
2025-11-04 13:58:21 +0100

    add: int cast

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 12:29:23 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/30 23:48:54 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/11/04 13:54:40 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,7 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
 
 void	ScalarConverter::convert(const std::string& str) {
 	std::cout << "CONVERT :" << std::endl;
-	// std::cout << str << std::endl;
-	// std::cout << const basic_string<char>str << std::endl;
 
-	int i = static_cast<int>(str);
-    char c = static_cast<char>(i);
-    float f = static_cast<float>(str);
-    double d = static_cast<double>(str);
-
-    if (std::isprint(c))
-        std::cout << "char: '" << c << "'" << std::endl;
-    else
-        std::cout << "char: Non displayable" << std::endl;
-
-    std::cout << "int: " << i << std::endl;
-    std::cout << "float: " << f << "f" << std::endl;
-    std::cout << "double: " << d << std::endl;
-
-	std::cout << "char: impossible\n";
-	std::cout << "int: impossible\n";
-	std::cout << "float: impossible\n";
-	std::cout << "double: impossible\n";
+	int value = std::atoi(str.c_str());
+	std::cout << "INT : " << value << std::endl;
 }
