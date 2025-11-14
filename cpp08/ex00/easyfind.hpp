16c5ce22717a2cfdf12140efec50c356a2fb7f18
juhanse <julien.hanse12@gmail.com>
2025-11-14 19:54:00 +0100

    add: cpp08 ex00

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 00:08:12 by juhanse           #+#    #+#             */
+/*   Updated: 2025/11/14 19:53:41 by juhanse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <vector>
+# include <iostream>
+
+template <typename T>
+int	easyfind(T container, int n) {
+	
+}
+
+#endif
