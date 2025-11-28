7c639e70c939b1421523962c6f4073d4e8236749
juhanse <julien.hanse12@gmail.com>
2025-11-28 18:11:29 +0100

    add: main test easyfind

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,12 +6,26 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:07:54 by juhanse           #+#    #+#             */
-/*   Updated: 2025/11/14 19:44:51 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/11/28 18:09:20 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
 int main(void) {
+	std::vector<int> vec;
+
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(30);
+	vec.push_back(40);
+
+	try {
+		std::cout << "Found: " << easyfind(vec, 30) << std::endl;
+		std::cout << "Found: " << easyfind(vec, 50) << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
 	return (0);
 }
