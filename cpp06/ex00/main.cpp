88bfeaf1bdad70d48fe140161ddf4726c381c3d3
juhanse <julien.hanse12@gmail.com>
2025-11-28 17:57:17 +0100

    add: enhanced tests

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,16 +6,32 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/22 21:48:08 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/11/28 17:55:16 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
 int	main(void) {
-	ScalarConverter::convert("c");
-	ScalarConverter::convert("42");
-	ScalarConverter::convert("42.0f");
-	ScalarConverter::convert("42.0");
+	{
+		std::cout << "----- TEST 1 -----" << std::endl;
+		std::cout << "c" << std::endl;
+		ScalarConverter::convert("c");
+	}
+	{
+		std::cout << "----- TEST 2 -----" << std::endl;
+		std::cout << "42" << std::endl;
+		ScalarConverter::convert("42");
+	}
+	{
+		std::cout << "----- TEST 3 -----" << std::endl;
+		std::cout << "42.0f" << std::endl;
+		ScalarConverter::convert("42.0f");
+	}
+	{
+		std::cout << "----- TEST 4 -----" << std::endl;
+		std::cout << "42.0" << std::endl;
+		ScalarConverter::convert("42.0");
+	}
 	return (0);
 }
