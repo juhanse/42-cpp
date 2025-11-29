990b1a99fed48bb0de93f8886020d4ed014642f9
juhanse <julien.hanse12@gmail.com>
2025-11-29 23:59:51 +0100

    add: real main

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,32 +6,20 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/11/28 17:55:16 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/11/29 23:59:15 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-int	main(void) {
-	{
-		std::cout << "----- TEST 1 -----" << std::endl;
-		std::cout << "c" << std::endl;
-		ScalarConverter::convert("c");
-	}
-	{
-		std::cout << "----- TEST 2 -----" << std::endl;
-		std::cout << "42" << std::endl;
-		ScalarConverter::convert("42");
-	}
-	{
-		std::cout << "----- TEST 3 -----" << std::endl;
-		std::cout << "42.0f" << std::endl;
-		ScalarConverter::convert("42.0f");
-	}
-	{
-		std::cout << "----- TEST 4 -----" << std::endl;
-		std::cout << "42.0" << std::endl;
-		ScalarConverter::convert("42.0");
+int	main(int ac, char **av) {
+	if (ac > 1) {
+		int i = 1;
+		while (i < ac) {
+			std::cout << "----- " << av[i] << " -----" << std::endl;
+			ScalarConverter::convert(av[i]);
+			i++;
+		}
 	}
 	return (0);
 }
