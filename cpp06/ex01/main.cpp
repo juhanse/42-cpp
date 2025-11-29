85b1724eaec7e5b0117789837f096b0a3072282e
juhanse <julien.hanse12@gmail.com>
2025-11-30 00:19:12 +0100

    add: main tests

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,12 +6,27 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:28:29 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/22 21:50:35 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/11/30 00:16:42 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
 int	main(void) {
+	Data *data = new Data;
+	data->firstname = "Julien";
+	data->lastname = "Hanse";
+	data->age = 42;
+
+	uintptr_t raw = Serializer::serialize(data);
+	Data *serialized = Serializer::deserialize(raw);
+
+	std::cout << "Original Data address: " << data << std::endl;
+	std::cout << "Serialized Data address: " << serialized << std::endl;
+	std::cout << "Firstname: " << serialized->firstname << std::endl;
+	std::cout << "Lastname: " << serialized->lastname << std::endl;
+	std::cout << "Age: " << serialized->age << std::endl;
+
+	delete data;
 	return (0);
 }
