4eb5dea8b94f8ab7165ef12a34758f08d29e173d
juhanse <julien.hanse12@gmail.com>
2025-12-01 16:05:10 +0100

    fix: template function

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:08:12 by juhanse           #+#    #+#             */
-/*   Updated: 2025/12/01 15:40:08 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/01 16:03:30 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include <iostream>
 #include <string>
 
-template <typename T>
-void	iter(T* array, const size_t length, void (*function)(T&)) {
-	for (size_t i = 0; i < length; i++) {
-		function(array[i]);
+template<typename T, typename Func>
+void	iter(T* array, const size_t length, Func function) {
+    for (size_t i = 0; i < length; ++i) {
+        function(array[i]);
 	}
 }
