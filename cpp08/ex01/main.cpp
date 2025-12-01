d3831feaf171ec2b2aa915b7385cdf41a484a985
juhanse <julien.hanse12@gmail.com>
2025-12-02 00:25:13 +0100

    add: enhanced tests

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -25,5 +25,27 @@ int	main(void) {
 		std::cout << sp.shortestSpan() << std::endl;
 		std::cout << sp.longestSpan() << std::endl;
 	}
+	{
+		Span sp = Span(10000);
+
+		for (int i = 0; i < 10000; i++) {
+			sp.addNumber(i * 2);
+		}
+
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	{
+		Span sp = Span(3);
+
+		try {
+			sp.addNumber(1);
+			sp.addNumber(2);
+			sp.addNumber(3);
+			sp.addNumber(4);
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
 	return (0);
 }
