e234cd11fd9911bc8c7fcb6bdb3d747c15b1822d
juhanse <julien.hanse12@gmail.com>
2025-12-01 16:44:25 +0100

    add: canonical form

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,26 +6,27 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 18:12:26 by juhanse           #+#    #+#             */
-/*   Updated: 2025/11/28 18:15:27 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/01 16:44:03 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SPAN_HPP
-#define SPAN_HPP
+#pragma once
 
 #include <iostream>
 
 class Span {
 	private:
-		unsigned int _N;
+		unsigned int	_N;
 
 	public:
+		Span();
 		Span(unsigned int N);
+		Span(const Span& copy);
 		~Span();
+
+		Span& operator=(const Span& other);
 
 		void addNumber(int number);
 		int shortestSpan();
 		int longestSpan();
 };
-
-#endif
