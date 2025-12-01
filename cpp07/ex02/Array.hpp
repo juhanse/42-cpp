05b57e7c40c4ae675158594142388049aecc20e2
juhanse <julien.hanse12@gmail.com>
2025-12-01 15:42:36 +0100

    add: pragma once

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,21 +6,21 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:08:12 by juhanse           #+#    #+#             */
-/*   Updated: 2025/10/21 00:29:08 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/01 15:42:22 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_HPP
-#define ARRAY_HPP
+#pragma once
 
-# include <iostream>
-# include <exception>
+#include <iostream>
+#include <exception>
 
 template <typename T>
 class Array {
 	private:
-		T*              _array;
-		size_t          _size;
+		T*		_array;
+		size_t	_size;
+
 	public:
 		Array();
 		Array(unsigned int n);
@@ -40,5 +40,3 @@ class Array {
 };
 
 #include "Array.tpp"
-
-#endif
