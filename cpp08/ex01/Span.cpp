550b49b437cc08e6e79d2ce21bbe3fb3714b4810
juhanse <julien.hanse12@gmail.com>
2025-12-01 16:46:47 +0100

    add: canonical form

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,31 +6,28 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 18:13:54 by juhanse           #+#    #+#             */
-/*   Updated: 2025/11/28 18:15:41 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/01 16:45:10 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span(unsigned int N) : _N(N)
-{
+Span::Span() : _N(1) {};
+
+Span::Span(unsigned int N) : _N(N) {
 }
 
-Span::~Span()
-{
+Span::~Span() {
 }
 
-void Span::addNumber(int number)
-{
+void Span::addNumber(int number) {
 	(void)number;
 }
 
-int Span::shortestSpan()
-{
+int Span::shortestSpan() {
 	return (0);
 }
 
-int Span::longestSpan()
-{
+int Span::longestSpan() {
 	return (0);
 }
