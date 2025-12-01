354fab9b06a67f9af9966401311fc8e28e76b702
juhanse <julien.hanse12@gmail.com>
2025-12-01 16:41:23 +0100

    add: pragma once

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,23 +6,23 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:08:12 by juhanse           #+#    #+#             */
-/*   Updated: 2025/11/28 18:09:50 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/01 16:40:42 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EASYFIND_HPP
-# define EASYFIND_HPP
+#pragma once
 
-# include <vector>
-# include <iostream>
-# include <algorithm>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
 template <typename T>
 int	easyfind(T c, int n) {
 	typename T::iterator it = std::find(c.begin(), c.end(), n);
-	if (it != c.end())
-		return (*it);
-	throw std::runtime_error("Value not found in container");
-}
 
-#endif
+	if (it != c.end()) {
+		return (*it);
+	} else {
+		throw std::runtime_error("Value not found in container");
+	}
+}
