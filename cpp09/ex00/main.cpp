b7f582351c2ece005626b750360e25367208c910
juhanse <julien.hanse12@gmail.com>
2025-12-05 12:26:39 +0100

    add: take main args

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,12 +6,20 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:07:54 by juhanse           #+#    #+#             */
-/*   Updated: 2025/12/02 16:12:34 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/05 12:26:01 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main(void) {
+int main(int ac, char **av) {
+	if (ac != 2) {
+		std::cerr << "Error: could not open file." << std::endl;
+		return (1);
+	}
+
+	BitcoinExchange btc;
+	btc.loadData("data.csv");
+	btc.processFile(av[1]);
 	return (0);
 }
