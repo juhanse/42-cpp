60c581eda50c0a78b052e4841bbc2449d2435080
juhanse <julien.hanse12@gmail.com>
2025-12-05 12:30:05 +0100

    add: loadData member function

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:12:59 by juhanse           #+#    #+#             */
-/*   Updated: 2025/12/05 12:27:19 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/05 12:29:53 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,23 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
 		this->dataMap = other.dataMap;
 	}
 	return *this;
+}
+
+void BitcoinExchange::loadData(const std::string& filename) {
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		throw std::runtime_error("Could not open data file.");
+	}
+
+	std::string line;
+	while (std::getline(file, line)) {
+		std::istringstream ss(line);
+		std::string date;
+		float value;
+
+		if (std::getline(ss, date, ',') && ss >> value) {
+			dataMap[date] = value;
+		}
+	}
+	file.close();
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,13 +6,16 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:08:12 by juhanse           #+#    #+#             */
-/*   Updated: 2025/12/05 12:27:38 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/12/05 12:29:19 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <exception>
 #include <map>
 
@@ -26,4 +29,6 @@ class BitcoinExchange {
 		~BitcoinExchange();
 
 		BitcoinExchange& operator=(const BitcoinExchange& other);
+
+		void loadData(const std::string& filename);
 };
