452d108888ec9a7b1495ca17c351369ab4307184
juhanse <julien.hanse12@gmail.com>
2026-01-09 22:09:19 +0100

    add: stack rpn

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -6,15 +6,20 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:08:12 by juhanse           #+#    #+#             */
-/*   Updated: 2025/12/06 15:40:20 by juhanse          ###   ########.fr       */
+/*   Updated: 2026/01/09 22:08:34 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
+# include <iostream>
+# include <string>
+# include <stack>
 
 class RPN {
+	private:
+		std::stack<double> _stack;
+
 	public:
 		RPN();
 		RPN(const RPN& copy);
