3a5db18aa198e55d458f7c95160766bdaaaf43d2
juhanse <julien.hanse12@gmail.com>
2026-01-12 19:57:53 +0100

    add: stack calculate

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:12:59 by juhanse           #+#    #+#             */
-/*   Updated: 2026/01/12 19:48:15 by juhanse          ###   ########.fr       */
+/*   Updated: 2026/01/12 19:56:51 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,14 @@ RPN::RPN(const RPN& other) {
 }
 
 RPN& RPN::operator=(const RPN& other) {
+	(void)other;
 	return *this;
 }
 
-void RPN::calculate(const std::string& filename) {
-	
+int	RPN::calculate(const std::string& expression) {
+	std::stack<int> stack;
+	std::istringstream iss(expression);
+	std::string token;
+
+	return stack.top();
 }
