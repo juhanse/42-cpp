2d92152352b1aa95433c66ad3b2ab87f9ab92440
juhanse <julien.hanse12@gmail.com>
2026-01-17 01:35:52 +0100

    fix: operation stack

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:12:59 by juhanse           #+#    #+#             */
-/*   Updated: 2026/01/13 15:05:08 by juhanse          ###   ########.fr       */
+/*   Updated: 2026/01/17 01:33:55 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,59 @@ RPN& RPN::operator=(const RPN& other) {
 	return *this;
 }
 
-int RPN::operation(const std::string& operator) {
-	if (operator == '*' || operator == '/' || operator == '-' || operator == '+') {
-		return (1);
+int RPN::result() const {
+	if (_stack.empty()) {
+		throw std::runtime_error("Error: Stack is empty");
 	}
-	return (0);
+
+	return _stack.top();
 }
 
-int	RPN::calculate(const std::string& expression) {
-	std::stack<int> stack;
+void RPN::operation(const std::string& token) {
+	int result;
+
+	_b = _stack.top();
+	_stack.pop();
+	_a = _stack.top();
+	_stack.pop();
+
+	if (token == "+") {
+		result = _a + _b;
+	} else if (token == "-") {
+		result = _a - _b;
+	} else if (token == "*") {
+		result = _a * _b;
+	} else if (token == "/") {
+		if (_b == 0) {
+			throw std::runtime_error("Error: Division by zero");
+		}
+		result = _a / _b;
+	} else {
+		throw std::runtime_error("Error: Unknown operator");
+	}
+
+	_stack.push(result);
+}
+
+void RPN::calculate(const std::string& expression) {
 	std::istringstream iss(expression);
 	std::string token;
 
-	return stack.top();
+	while (iss >> token) {
+		if (token.length() == 1 && std::isdigit(token[0])) {
+			_stack.push(token[0] - '0');
+		} else if (token == "+" || token == "-" || token == "*" || token == "/") {
+			try {
+				operation(token);
+			} catch (const std::exception& e) {
+				throw;
+			}
+		} else {
+			throw std::runtime_error("Error: Invalid token");
+		}
+	}
+
+	if (_stack.size() != 1) {
+		throw std::runtime_error("Error: Invalid RPN expression");
+	}
 }
