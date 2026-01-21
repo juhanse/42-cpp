3c46bbe2064a9dfeab92a82fe34690a967940aae
juhanse <julien.hanse12@gmail.com>
2026-01-21 14:27:30 +0100

    refactor: call PmergeMe methods

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 00:07:54 by juhanse           #+#    #+#             */
-/*   Updated: 2026/01/21 11:36:05 by juhanse          ###   ########.fr       */
+/*   Updated: 2026/01/21 14:08:47 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int main(int ac, char **av) {
 	PmergeMe pm;
 
 	try {
-		pm.merge_insert(ac, av);
+		pm.initialize(ac, av);
+		pm.sort();
+		pm.display();
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
 		return (1);
